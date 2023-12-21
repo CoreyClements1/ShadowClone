b156632f7e7f2b62b679029c34b658a8b917f7ae
CoreyClements1 <coreyclements@knights.ucf.edu>
2023-12-21 12:24:49 -0500

    Render As Needed
    Renders tiles as needed based on player location

--- ShadowClone/Assets/Scripts/Generation/GroundGenerator.cs
+++ ShadowClone/Assets/Scripts/Generation/GroundGenerator.cs
@@ -13,15 +13,18 @@ public class GroundGenerator : MonoBehaviour
 
 
     [Header("General")]
-    [SerializeField] private int chunkSize = 16;
-    [SerializeField] private int worldWidthChunks = 5;
+    [SerializeField] private int viewDistance;
+    [SerializeField] private int seed;
+    [SerializeField] private Transform playerTransform;
+    private Vector2Int lastPlayerLoc;
+
+    [Header("Tiles")]
     [SerializeField] private Tilemap tilemapHigh;
     [SerializeField] private Tilemap tilemapMid;
     [SerializeField] private Tilemap tilemapLow;
     [SerializeField] private RuleTile ruleTileLowType;
     [SerializeField] private RuleTile ruleTileHighType;
     [SerializeField] private Tile basicTile;
-    [SerializeField] private int seed;
 
     [Header("Land")]
     [SerializeField] private float scale;
@@ -48,7 +51,7 @@ public class GroundGenerator : MonoBehaviour
     void Start()
     //--------------------------------------//
     {
-        GenerateGround();
+        GenInitialGround();
 
     } // END Start
 
@@ -58,9 +61,90 @@ public class GroundGenerator : MonoBehaviour
     void Update()
     //--------------------------------------//
     {
-
+        CheckNeedGeneration();
 
     } // END Update
+
+
+    #endregion
+
+
+    #region PLAYER CHECK
+
+
+    // Checks if generation is required based on player movements
+    //--------------------------------------//
+    private void CheckNeedGeneration()
+    //--------------------------------------//
+    {
+        Vector2Int playerLoc = new Vector2Int(Mathf.FloorToInt(playerTransform.position.x), Mathf.FloorToInt(playerTransform.position.y));
+        int halfView = Mathf.FloorToInt((float)viewDistance / 2f);
+
+        // If player has entered new grid segment...
+        if (playerLoc != lastPlayerLoc)
+        {
+            // Render columns to the right
+            if (playerLoc.x > lastPlayerLoc.x)
+            {
+                // Render all columns between player last loc and player new loc
+                int remainingRenders = Mathf.Abs(playerLoc.x - lastPlayerLoc.x);
+                for (int r = 0; r < remainingRenders; r++)
+                {
+                    int i = playerLoc.x + halfView - r;
+                    for (int j = playerLoc.y - halfView; j < playerLoc.y + halfView + 1; j++)
+                    {
+                        GenerateGroundAtLoc(new Vector2Int(i, j));
+                    }
+                }
+            }
+            // Render column to the left
+            else if (playerLoc.x < lastPlayerLoc.x)
+            {
+                // Render all columns between player last loc and player new loc
+                int remainingRenders = Mathf.Abs(playerLoc.x - lastPlayerLoc.x);
+                for (int r = 0; r < remainingRenders; r++)
+                {
+                    int i = playerLoc.x - halfView + r;
+                    for (int j = playerLoc.y - halfView; j < playerLoc.y + halfView + 1; j++)
+                    {
+                        GenerateGroundAtLoc(new Vector2Int(i, j));
+                    }
+                }
+            }
+
+            // Render row above
+            if (playerLoc.y > lastPlayerLoc.y)
+            {
+                // Render all rows between player last loc and player new loc
+                int remainingRenders = Mathf.Abs(playerLoc.y - lastPlayerLoc.y);
+                for (int r = 0; r < remainingRenders; r++)
+                {
+                    int j = playerLoc.y + halfView - r;
+                    for (int i = playerLoc.x - halfView; i < playerLoc.x + halfView + 1; i++)
+                    {
+                        GenerateGroundAtLoc(new Vector2Int(i, j));
+                    }
+                }
+            }
+            // Render row below
+            else if (playerLoc.y < lastPlayerLoc.y)
+            {
+                // Render all rows between player last loc and player new loc
+                int remainingRenders = Mathf.Abs(playerLoc.y - lastPlayerLoc.y);
+                for (int r = 0; r < remainingRenders; r++)
+                {
+                    int j = playerLoc.y - halfView + r;
+                    for (int i = playerLoc.x - halfView; i < playerLoc.x + halfView + 1; i++)
+                    {
+                        GenerateGroundAtLoc(new Vector2Int(i, j));
+                    }
+                }
+            }
+        }
+
+        lastPlayerLoc = playerLoc;
+    
+    } // END CheckNeedGeneration
 
 
     #endregion
@@ -69,36 +153,49 @@ public class GroundGenerator : MonoBehaviour
     #region GROUND GENERATION
 
 
-    // Generates the ground
+    // Generates the initial ground around player
     //--------------------------------------//
-    private void GenerateGround()
+    private void GenInitialGround()
     //--------------------------------------//
     {
-        for (int i = 0; i < 500; i++)
+        Vector2Int playerLoc = new Vector2Int(Mathf.FloorToInt(playerTransform.position.x), Mathf.FloorToInt(playerTransform.position.y));
+        int halfView = Mathf.FloorToInt((float)viewDistance / 2f);
+
+        for (int i = playerLoc.x - halfView; i < playerLoc.x + halfView + 1; i++)
         {
-            for (int j = 0; j < 500; j++)
+            for (int j = playerLoc.y - halfView; j < playerLoc.y + halfView + 1; j++)
             {
-                bool genRiver = GetShouldGenRiver(new Vector2(i, j));
-
-                float perlinOffset = NoiseHelper.Perlin2D(new Vector2(i, j), seed * 2, edgeOffsetScale);
-                float perlinVal = NoiseHelper.Perlin2D(new Vector2(i + (perlinOffset * edgeOffsetAmt), j + (perlinOffset * edgeOffsetAmt)), seed, scale);
-
-                if (perlinVal < .35f)
-                {
-                    tilemapLow.SetTile(new Vector3Int(i, j, 0), ruleTileLowType);
-                }
-                else if (perlinVal < .65f)
-                {
-                    //groundTilemap.SetTile(new Vector3Int(i, j, 0), basicTile);
-                }
-                else
-                {
-                    tilemapHigh.SetTile(new Vector3Int(i, j, 0), ruleTileHighType);
-                }
-
-                tilemapMid.SetTile(new Vector3Int(i, j, 0), basicTile);
+                GenerateGroundAtLoc(new Vector2Int(i, j));
             }
         }
+
+    } // END GenInitialGround
+
+
+    // Generates the ground
+    //--------------------------------------//
+    private void GenerateGroundAtLoc(Vector2Int loc)
+    //--------------------------------------//
+    {
+        bool genRiver = GetShouldGenRiver(new Vector2(loc.x, loc.y));
+
+        float perlinOffset = NoiseHelper.Perlin2D(new Vector2(loc.x, loc.y), seed * 2, edgeOffsetScale);
+        float perlinVal = NoiseHelper.Perlin2D(new Vector2(loc.x + (perlinOffset * edgeOffsetAmt), loc.y + (perlinOffset * edgeOffsetAmt)), seed, scale);
+
+        if (perlinVal < .35f)
+        {
+            tilemapLow.SetTile(new Vector3Int(loc.x, loc.y, 0), ruleTileLowType);
+        }
+        else if (perlinVal < .65f)
+        {
+            //groundTilemap.SetTile(new Vector3Int(i, j, 0), basicTile);
+        }
+        else
+        {
+            tilemapHigh.SetTile(new Vector3Int(loc.x, loc.y, 0), ruleTileHighType);
+        }
+
+        tilemapMid.SetTile(new Vector3Int(loc.x, loc.y, 0), basicTile);
 
     } // END GenerateGround
 
